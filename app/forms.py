384b844b4c545253704481deb1a7e1a9742a8ec8
selim1mm <selim@1mmcorp.com>
2015-05-30 20:36:10 -0400

    forms started for creating a user and a game, need to finish the start_game one

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,16 @@
-__author__ = 'selim'
-
 from flask.ext.wtf import Form
 from wtforms import StringField, BooleanField
 from wtforms.validators import DataRequired
 
-class LoginForm(Form):
-    openid = StringField('openid', validators=[DataRequired()])
-    remember_me = BooleanField('remember_me', default=False)
+
+class SignUpForm(Form):
+    name = StringField('name', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired()])
+
+
+class StartGameForm(Form):
+    player1 = StringField('player1')
+    player2 = StringField('player2')
+    player3 = StringField('player3')
+    player4 = StringField('player4')
+    player5 = StringField('player5')
--- app/views.py
+++ app/views.py
@@ -1,23 +1,69 @@
 from flask import render_template, flash, redirect
 from app import app
-from .forms import LoginForm
+from forms import *
+import models
+from app import db
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Miguel'}  # fake user
     return render_template('index.html',
-                           title='Home',
-                           user=user)
+                           title='Home')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
+
+@app.route('/sign_up', methods=['GET', 'POST'])
+def sign_up():
+    form = SignUpForm()
     if form.validate_on_submit():
-        flash('Login requested for OpenID="%s", remember_me=%s' %
-              (form.openid.data, str(form.remember_me.data)))
+        flash('Signup requested for Name="{}", Email={}'.format(form.name.data, form.email.data))
+
+        # Add the new player to the database
+        new_player = models.Player(name=form.name.data, email=form.email.data)
+        db.session.add(new_player)
+        db.session.commit()
+
         return redirect('/index')
-    return render_template('login.html',
-                           title='Sign In',
-                           form=form,
-                           providers=app.config['OPENID_PROVIDERS'])
+    return render_template('sign_up.html',
+                           title='Sign Up',
+                           form=form)
+
+
+@app.route('/start_game', methods=['GET', 'POST'])
+def start_game():
+    form = StartGameForm()
+
+    if form.validate_on_submit():
+
+        players_email = [form.player1.data, form.player2.data, form.player3.data, form.player4.data, form.player5.data]
+        players_email = [email for email in players_email if len(email) > 0]
+        flash('Start a game requested for players {}'.format(' - '.join(players_email)))
+
+        # Instanciate a new game
+        new_game = models.Game(isOn=True)
+        db.session.add(new_game)
+        #db.session.commit()
+
+        # Match players to game
+        players = list()
+        for player_email in players_email:
+            players.extend(models.Player.query.filter(models.Player.email==player_email).all())
+            ################## FINISH HERE
+
+        print players
+
+        new_game_id = 10
+        return redirect('/game/{}'.format(new_game_id))
+    return render_template('start_game.html',
+                           title='Start a new game',
+                           form=form)
+
+
+@app.route('/game', methods=['GET', 'POST'])
+@app.route('/game/<game_id>', methods=['GET', 'POST'])
+def game(game_id=0):
+
+    print 'Game', game_id
+
+    return render_template('game.html',
+                           title='Game {}'.format(game_id),
+                           game_id=game_id)
